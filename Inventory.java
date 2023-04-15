02eeb782ba88870525ccc73dbe21fb990e612097
gevyndo <gevyndo18@gmail.com>
2023-04-15 15:54:04 +0700

    Update Inventory.java

---
+++ Inventory.java
@@ -0,0 +1,9 @@
+public class Inventory {
+    private String name;
+    private int count;
+
+    public Inventory(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+}
