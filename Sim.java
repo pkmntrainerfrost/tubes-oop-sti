df46f4a1b37130f8b64bada54949b68aec17964a
gevyndo <gevyndo18@gmail.com>
2023-04-15 15:25:04 +0700

    Update Sim.java

--- Sim.java
+++ Sim.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Sim {
 
@@ -9,15 +11,15 @@ public class Sim {
     private int kesehatan=80;
     private int uang=100;
     private String status="";
-    private String inventory="";
+    private Set<String> inventory;
 
     private static final String[] Jobs = {"Badut Sulap", "Koki", "Polisi", "Programmer", "Dokter", "Guru", "Penulis", "Musisi"};
     private static final Random RANDOM = new Random();
 
     public Sim(String name) {
-        // Konstruktor
         this.name = name;
         this.job = JobGenerator();
+        this.inventory = new HashSet<>();;
     }
     public String JobGenerator() {
         int index = RANDOM.nextInt(Jobs.length);
@@ -42,17 +44,44 @@ public class Sim {
     public String getStatus() {
         return this.status;
     }
-    public String getInventory() {
-        return this.inventory;
-    }
     public int getUang() {
         return this.uang;
+    }
+    public int getKesehatan(){
+        return this.kesehatan;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setKekenyangan(int kekenyangan) {
+        this.kekenyangan = kekenyangan;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    public void setUang(int uang) {
+        this.uang = uang;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Set<String> getInventory() {
+        return this.inventory;
+    }
+    public void addItemToInventory(String item) {
+        this.inventory.add(item);
     }
     public void work(){
         mood-=10;
         kekenyangan-=10;
     }
-    public int getKesehatan(){
-        return this.kesehatan;
-    }
+
 }
