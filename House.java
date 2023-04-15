3d4af715849a490ee6320dd4969d8312f4b50162
reswaratrista <18221122@std.stei.itb.ac.id>
2023-04-15 12:56:38 +0700

    update house

--- House.java
+++ House.java
@@ -1,30 +1,32 @@
 import java.util.*;
-public class House {
+public class House extends GridObject{
     private Grid grid;
     private List<Room> rooms; 
 
     //konstruktor
-    public House(int x, int y){
-        this.grid = new Grid(x, y);
-        this.rooms = new ArrayList<>(); 
+    public House (Point p, Grid grid){
+        super(p, 6, 6);
+        this.grid = grid;
         try {
+            this.rooms = new ArrayList<Room>(); 
             // Menambahkan kamar pertama dengan ukuran 6x6 pada grid
-            Point p = new Point(x, y); //point awal rumah pakai inputan
             Room room = new Room(p, 6, 6); 
             grid.addObject(room);
-            rooms.add(room);
+            this.rooms.add(room);
         } catch (PositionOccupiedException | PositionOutOfBoundsException e) {
             e.printStackTrace();
         }
     }
-    // sementara 
-    boolean isCharaInHouse = true;
 
-    public void houseUpgrade(Room newRoom, Room neighbor, String dir) throws Exception{
+    public void houseUpgrade(Room newRoom, String dir) throws Exception{
+        // sementara 
+        Room neighbor = rooms.get(0);
+        boolean isCharaInHouse = true;
         if (isCharaInHouse){
             try {
                 // Memastikan bahwa room yang akan ditambahkan terhubung dengan neighbornya
-                if (neighbor == null || (dir.equals("top") && neighbor.getMaximumY() == newRoom.getMinimumY() - 1) || (dir.equals("under") && neighbor.getMinimumY() == newRoom.getMaximumY() + 1) || (dir.equals("left") && neighbor.getMaximumX() == newRoom.getMinimumX() - 1) || (dir.equals("right") && neighbor.getMinimumX() == newRoom.getMaximumX() + 1)) {
+                if ((dir.equals("top") && neighbor.getMaximumY() == newRoom.getMinimumY()) || (dir.equals("under") && neighbor.getMinimumY() == newRoom.getMaximumY()) || (dir.equals("left") && neighbor.getMaximumX() == newRoom.getMinimumX()) || (dir.equals("right") && neighbor.getMinimumX() == newRoom.getMaximumX())) {
+                    setHouseSize(6, dir);
                     grid.addObject(newRoom);
                     rooms.add(newRoom);
                     //NOT YET: makan waktu untuk aksi
@@ -35,10 +37,22 @@ public class House {
             } catch (PositionOccupiedException | PositionOutOfBoundsException e) {
                 e.printStackTrace();
             }
+            rooms.add(newRoom);
         }
         else{
             throw new Exception("You are not in your house!");
         }
+    }
+
+    public void setHouseSize(int num, String dir){
+        if (dir.equals("right") | dir.equals("left")){
+            grid.setGridX(6);
+            setLength(num);
+        }
+        if (dir.equals("top") | dir.equals("under")){
+            grid.setGridY(6);
+            setHeight(num);
+        }        
     }
 
     public Grid getGrid() { //pindah ke grid (?)
@@ -48,10 +62,13 @@ public class House {
     public List<Room> getRoomList() {
         return rooms;
     }
+
 }
 
+//sesuaikan aja yg bikin room nnt ini diapus, tp namanya samain kalo bisa
 class Room extends GridObject {
     public Room(Point point, int length, int height) {
         super(point, length, height);
     }
 }
+
