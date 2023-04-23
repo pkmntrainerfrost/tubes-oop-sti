cfbef553e271632ade35c60b26da4935d99a5c1a
MUHAMAD PEDRO <muhamadpdr03@gmail.com>
2023-04-23 18:16:16 +0700

    add moveRoom dan editRoom

--- Room.java
+++ Room.java
@@ -38,4 +38,88 @@ public class Room extends GridObject {
         return objectGrid.getObjects();
     }
 
-}
+    // Metode untuk memindahkan Sim ke ruangan lain di rumah
+    public void moveRoom(Sim sim, Room newRoom) {
+        // Memeriksa apakah Sim sedang berada di dalam rumah ini
+        if (sim.getCurrentRoom() == this) {
+            // Memindahkan Sim ke ruangan baru
+            sim.setCurrentRoom(newRoom);
+            System.out.println("Sim berhasil berpindah ke " + newRoom.getName());
+        } else {
+            System.out.println("Sim tidak berada di dalam rumah ini!");
+        }
+    }
+
+    // Metode untuk mengedit barang yang ada di dalam room
+    public void editRoom() {
+        Scanner input = new Scanner(System.in);
+        // Memeriksa apakah Sim sedang berada di dalam ruangan ini
+        if (Sim.getCurrentSim().getCurrentRoom() == this) {
+            System.out.println("Pilih opsi:");
+            System.out.println("1. Pembelian barang baru");
+            System.out.println("2. Pemindahan barang");
+
+            int option = input.nextInt();
+            switch (option) {
+                case 1:
+                    // Meminta input barang yang ingin dibeli
+                    System.out.println("Masukkan nama barang:");
+                    String itemName = input.next();
+                    System.out.println("Masukkan harga barang:");
+                    double itemPrice = input.nextDouble();
+
+                    // Membuat objek Item baru
+                    Item newItem = new Item(itemName, itemPrice);
+
+                    // Menambahkan objek Item baru ke dalam objekGrid
+                    objectGrid.addObject(newItem);
+                    System.out.println("Barang " + itemName + " berhasil ditambahkan ke dalam ruangan " + name);
+                    break;
+                case 2:
+                    // Meminta input barang yang ingin dipindahkan
+                    System.out.println("Masukkan nama barang yang ingin dipindahkan:");
+                    String itemToMove = input.next();
+
+                    // Mencari objek Item yang ingin dipindahkan
+                    GridObject item = objectGrid.findObjectByName(itemToMove);
+
+                    // Memeriksa apakah objek Item ditemukan
+                    if (item == null) {
+                        System.out.println("Barang " + itemToMove + " tidak ditemukan di dalam ruangan " + name);
+                    } else {
+                        // Memindahkan objek Item ke ruangan lain atau ke inventory Sim
+                        System.out.println("Pilih opsi:");
+                        System.out.println("1. Pindahkan ke ruangan lain");
+                        System.out.println("2. Pindahkan ke inventory Sim");
+                        int moveOption = input.nextInt();
+
+                        if (moveOption == 1) {
+                            // Meminta input ruangan tujuan
+                            System.out.println("Masukkan nama ruangan tujuan:");
+                            String destinationRoomName = input.next();
+
+                            // Mencari ruangan tujuan
+                            Room destinationRoom = Sim.getCurrentSim().getHouse().findRoomByName(destinationRoomName);
+
+                            // Memeriksa apakah ruangan tujuan ditemukan
+                            if (destinationRoom == null) {
+                                System.out.println("Ruangan " + destinationRoomName + " tidak ditemukan di dalam rumah.");
+                            } else {
+                                // Memindahkan objek Item ke ruangan tujuan
+                                objectGrid.removeObject(item);
+                                destinationRoom.getObjectGrid().addObject(item);
+                                System.out.println("Barang " + itemToMove + " berhasil dipindahkan ke ruangan " + destinationRoomName);
+                            }
+                        } else if (moveOption == 2) {
+                            // Memindahkan objek Item ke inventory Sim
+                            Sim.getCurrentSim().getInventory().addItem((Item)item);
+                            objectGrid.removeObject(item);
+                            System.out.println("Barang " + itemToMove + " berhasil dipindahkan ke inventory Sim");
+                        } else {
+                            System.out.println("Opsi yang dimasukkan tidak valid");
+                        }
+                    }
+            }
+        }
+    }
+}         
