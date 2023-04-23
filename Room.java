9ce7097f1e88ea6823311a10a07f03631c6bed07
MUHAMAD PEDRO <muhamadpdr03@gmail.com>
2023-04-23 17:32:52 +0700

    update room

--- Room.java
+++ Room.java
@@ -4,73 +4,38 @@ import java.util.List;
 
 public class Room extends GridObject {
 
-    // gk perlu, dihardcode soalnya jadinya (ruangan pasti 6x6), di house cmn makan 1x1
-
-    /*
-    private int panjangRuang;
-    private int lebarRuang;
-    */
-
-    // diganti grid
-    // private List<Object> daftarObjek;
-
+    // Atribut yang digunakan untuk menyimpan objek-objek yang berada di dalam room
     private Grid objectGrid;
 
+    // Atribut yang digunakan untuk menyimpan nama dari room
     private String name;
+
+    // Atribut yang digunakan untuk menyimpan status selesainya room
     private boolean finished;
 
+    // Konstruktor room dengan parameter titik awal, nama, dan status selesainya room
     public Room(Point point, String name, boolean finished) {
-        super(point, 1, 1);
-        objectGrid = new Grid(6,6);
+        // Memanggil konstruktor dari kelas induk (GridObject) dengan ukuran 6x6, karena room selalu memiliki ukuran 6x6
+        super(point, 6, 6);
+
+        // Membuat objek Grid dengan ukuran 6x6 untuk menyimpan objek-objek yang berada di dalam room
+        objectGrid = new Grid(6, 6);
+
+        // Mengatur nilai atribut name dengan parameter name yang diberikan
         this.name = name;
+
+        // Mengatur nilai atribut finished dengan parameter finished yang diberikan
         finished = false;
     }
 
+    // Metode untuk mengatur status selesainya room
     public void setFinished(boolean finished) {
         this.finished = finished;
     }
 
-    /*
-    public Room(Point point) {
-        super(point, length, width);
-        this.panjangRuang = length;
-        this.lebarRuang = width;
-        this.daftarObjek = new ArrayList<Object>();
-    }
-    */
-
-    // gk perlu, udah pasti 6x6
-
-    /*
-    public int getPanjangRuang() {
-        return panjangRuang;
-    }
-
-    public void setPanjangRuang(int panjangRuang) {
-        this.panjangRuang = panjangRuang;
-    }
-
-    public int getLebarRuang() {
-        return lebarRuang;
-    }
-
-    public void setLebarRuang(int lebarRuang) {
-        this.lebarRuang = lebarRuang;
-    }
-    */ 
-
+    // Metode untuk mendapatkan objek-objek yang berada di dalam room
     public List<GridObject> getObjects() {
         return objectGrid.getObjects();
     }
 
-    /*
-    public List<Object> getDaftarObject() {
-        return daftarObjek;
-    }
-
-    public void setDaftarObject(List<Object> daftarObjek) {
-        this.daftarObjek = daftarObjek;
-    }
-    */
-    
 }
