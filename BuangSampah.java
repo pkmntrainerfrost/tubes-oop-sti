4db35762df4292a8c21680462ff258610d75dac1
MUHAMAD PEDRO <muhamadpdr03@gmail.com>
2023-04-26 22:41:12 +0700

    add buangsampah

---
+++ BuangSampah.java
@@ -0,0 +1,14 @@
+public class BuangSampah extends SimActiveAction {
+    public BuangSampah(int duration, Sim sim) {
+        super(sim, duration);
+    }
+
+    @Override
+    public void acted() {
+        int time = getDuration() / 10;
+        for (int i = 0; i < time; i++) {
+            getSim().setMood(getSim().getMood() + 5);
+            getSim().setKesehatan(getSim().getKesehatan() + 5);
+        }
+    }
+}
