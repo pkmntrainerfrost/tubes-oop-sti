d5c65c074ab72326db0b0ceefc58f52b915f13b9
gevyndo <gevyndo18@gmail.com>
2023-04-29 20:01:08 +0700

    update

--- Sing.java
+++ Sing.java
@@ -1,12 +1,12 @@
 public class Sing extends SimActiveAction {
 
-    public Sing(Sim sim, int duration) {
-        super(sim, duration);
-    }
-
-    public void finish() {
-        int time = getDuration()/60;
-        getSim().setMood(getSim().getMood() + time * 5);
+    public Sing(Sim sim, int duration) { 
+        super(sim, duration); 
+    } 
+ 
+    public void finish() { 
+        int time = getDuration()/60; 
+        getSim().setMood(getSim().getMood() + time * 5); 
         getSim().setKekenyangan(getSim().getKekenyangan() - time * 5);
     }
 
