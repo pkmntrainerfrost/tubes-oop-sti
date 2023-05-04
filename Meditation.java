bb5d4e27f5e3cd5636f488e4e87c6aae15c79927
muhdastinf <muhammaddastinfauzi@gmail.com>
2023-05-04 22:35:27 +0700

    tes

--- Meditation.java
+++ Meditation.java
@@ -1,11 +1,17 @@
+import java.util.*;
+
 public class Meditation extends SimActiveAction {
     public void begin(Sim sim){
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("input meditation duration: ");
+        int duration = scan.nextInt();
+        setDuration(duration);
     }
 
     public void finish() {
         if (getSim().getCurrentRoom().getItemListInRoom().contains("Mattress")){
-            int time = getDuration()/600;
+            /* duration  */
+            int duration = getDuration()/10;      
             getSim().setMood(getSim().getMood() + time * 5);
             getSim().setKesehatan(getSim().getKesehatan() + time * 5);
         }
--- ReadBook.java
+++ ReadBook.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class ReadBook extends SimActiveAction {
 
     public void begin(Sim sim){
-        System.out.println("Masukkan durasi: ");
+        System.out.print("input readbook duration: ");
         Scanner scan = new Scanner(System.in);
         int duration = scan.nextInt();
+        setDuration(duration);
     }
 
     public void end(Sim sim) {
         if (getSim().getCurrentRoom().getItemListInRoom().contains("Rak buku")){
-            int time = getDuration()/60;
-            getSim().setMood(getSim().getMood() + time * 6);
-            getSim().setKekenyangan(getSim().getKekenyangan() - time * 6);
+            int duration = getDuration()/60;
+            getSim().setMood(getSim().getMood() + (time * 6));
+            getSim().setKekenyangan(getSim().getKekenyangan() - (time * 6));
         }
     }
-
 }
